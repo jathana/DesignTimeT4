ebdc6163be529dd6c8bd5a4f59c11cd1be7353cc
Athanasoulopoulos John, Qualco <JAthanasoulopoulos@qualco.eu>
2016-04-20 17:52:24 +0300

    commit

--- DesignTimeT4/TextTemplate1.cs
+++ DesignTimeT4/TextTemplate1.cs
@@ -2,7 +2,22 @@
 
 namespace DesignTimeT4
 {
-      public partial class QAddColumnToTableCR
+      // class begin
+      public partial class QAddColumnToTableCR : object
       {
-      }
+         #region fields
+         private string _ColumnName = "";
+         #endregion
+
+         #region properties
+
+         #endregion
+
+         #region constructors
+         #endregion
+
+
+
+
+      } // class end
 }
