7d16bf6b811e2723304802577392074bc5bcd0c6
Athanasoulopoulos John, Qualco <JAthanasoulopoulos@qualco.eu>
2016-04-21 18:07:38 +0300

    c

---
+++ CodeGenLib/IGenerateResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGenLib
+{
+   public interface IGenerateResult<T>
+   {
+      T Result { get; }
+      T this[string Key] { get; }
+   }
+}
--- DesignTimeT4/TextTemplate1.cs
+++ DesignTimeT4/TextTemplate1.cs
@@ -7,6 +7,26 @@ namespace DesignTimeT4
       {
          #region fields
          private string _ColumnName = "";
+         private string _ColumnName2 = "";
+         #endregion
+
+         #region properties
+
+         #endregion
+
+         #region constructors
+         #endregion
+
+
+
+
+      } // class end
+      // class begin
+      public partial class QAddColumnToTableCR2 : object
+      {
+         #region fields
+         private string _ColumnName = "";
+         private string _ColumnName2 = "";
          #endregion
 
          #region properties
